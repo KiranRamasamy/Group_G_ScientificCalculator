7e7e993e4031e7896be9d5c1bac99808b54e3f24
kiran Ramasamy <kiranramasamy@cmail.carleton.ca>
2020-06-12 20:39:09 -0400

    Added stack.c and modified stack.h

--- stack.h
+++ stack.h
@@ -1,6 +1,3 @@
-#include <stdio.h>
-#include <stdlib.h>
-
 typedef struct
 {
 	void **content;
@@ -20,45 +17,3 @@ int stackSize(Stack *s);
 
 void stackFree(Stack *s);
 
-void stackInit(Stack *s, int size)
-{
-	s->content = malloc(size * sizeof(void*));
-	s->size = size;
-	s->top = -1;
-}
-
-void stackPush(Stack *s, void* val)
-{
-	(s->top)++;
-	s->content[s->top] = val;
-}
-
-void* stackPop(Stack *s)
-{
-	void *ret = NULL;
-	if(s->top >= 0 && s->content != NULL)
-		ret = s->content[(s->top)--];
-	return ret;
-}
-
-void* stackTop(Stack *s)
-{
-	void *ret = NULL;
-	if(s->top >= 0 && s->content != NULL)
-		ret = s->content[s->top];
-	return ret;
-}
-
-void stackFree(Stack *s)
-{
-	if (s->content)
-		free(s->content);
-	s->content = NULL;
-	s->size = 0;
-	s->top = -1;
-}
-
-int stackSize(Stack *s)
-{
-	return s->top + 1;
-}
