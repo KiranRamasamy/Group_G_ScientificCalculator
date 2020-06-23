ffc76e67cb62227640d77dd36627ad976667f1a5
Tejaswini <65416167+tejaswinitummala@users.noreply.github.com>
2020-06-23 12:22:30 -0400

    Update postfix_test.h

--- test/include/postfix_test.h
+++ test/include/postfix_test.h
@@ -1,9 +1,9 @@
 /*
  * @file postfix_test.h
  *
- * @brief 
+ * @brief Collections of different variables needed for automated unit tests for postfix function
  *
- * @author 
+ * @author Tejaswini Tummala - tejaswinitummala@cmail.carleton.ca
  */
 
 #ifndef TEST_H
@@ -14,10 +14,27 @@
 #include<string.h>
 #include "../../include/stack.h"
 #include "../../src/stack.c"
+
 #define bool char
-#define false 0
 #define true 1
+#define false 0
+#define MAXPRECISION 20
+#define MAXTOKENLENGTH 512
+#define DEFAULTPRECISION 5
 #define FUNCTIONSEPARATOR "|"
+#define PI 3.141592653589793
+
+
+#ifndef NAN
+#define NAN (0.0/0.0)
+#endif
+
+#ifndef INFINITY
+#define INFINITY (1.0/0.0)
+#endif
+
+typedef double number;
+typedef char* token;
 
 typedef char* token;
 typedef enum
@@ -37,6 +54,21 @@ typedef enum
 	argsep,
 	invalid
  } Symbol;
+
+ struct Preferences
+{
+	struct Display
+	{
+		bool tokens;
+		bool postfix;
+	} display;
+	struct Mode
+	{
+		bool degrees;
+	} mode;
+	int precision;
+	int maxtokenlength;
+} prefs;
 
  typedef enum
 {
